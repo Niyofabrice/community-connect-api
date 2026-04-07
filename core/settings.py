3e3fd72db7d4d5320dfa66bf960d646dd876e3eb
Niyofabrice <nf.fabrice.niyonkuru@gmail.com>
2026-04-07 15:30:03 +0200

    changed email host to gmail smtp

--- core/settings.py
+++ core/settings.py
@@ -204,7 +204,7 @@ SIMPLE_JWT = {
 QUARANTINE_ROOT = 'quarantine/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
